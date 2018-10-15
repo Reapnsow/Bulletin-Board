56a8b9773a4203f36339c571f1d7b6be9c39036d
Reapnsow <johnnyjuarez67@gmail.com>
2018-10-14 23:59:37 -0700

    Updates

---
+++ precache-manifest.52b83d2c5ca065efb638de76bf841461.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "815a3d4b338105962b38",
+    "url": "/bulletin-board/static/js/runtime~main.815a3d4b.js"
+  },
+  {
+    "revision": "b64ecbfa4d83d9204816",
+    "url": "/bulletin-board/static/js/main.b64ecbfa.chunk.js"
+  },
+  {
+    "revision": "21a88d23535809ade69e",
+    "url": "/bulletin-board/static/js/1.21a88d23.chunk.js"
+  },
+  {
+    "revision": "b64ecbfa4d83d9204816",
+    "url": "/bulletin-board/static/css/main.2e8e7409.chunk.css"
+  },
+  {
+    "revision": "8ec431a32b4a626f170491b692ad5ebc",
+    "url": "/bulletin-board/index.html"
+  }
+];
